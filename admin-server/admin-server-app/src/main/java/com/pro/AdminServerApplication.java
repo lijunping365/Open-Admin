30803da2d8da7fb00d306db592e3b38f7ec1b39b
李俊平 <2544054976@qq.com>
2022-01-12 17:12:50 +0800

    feat:移除 logger 模块

--- admin-server/admin-server-app/src/main/java/com/pro/AdminServerApplication.java
+++ admin-server/admin-server-app/src/main/java/com/pro/AdminServerApplication.java
@@ -1,6 +1,5 @@
 package com.pro;
 
-import com.pro.starter.logger.annotation.EnableLogger;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lijunping on 2021/12/22
  */
 @EnableDubbo
-@EnableLogger
 @SpringBootApplication
 public class AdminServerApplication {
 
