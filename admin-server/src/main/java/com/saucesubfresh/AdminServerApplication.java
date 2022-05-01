efec6c653bdae028a9d1f0d1245d375dc57bc2f5
lijunping <2544054976@qq.com>
2022-05-01 17:35:14 +0800

    feat:优化

--- admin-server/src/main/java/com/saucesubfresh/AdminServerApplication.java
+++ admin-server/src/main/java/com/saucesubfresh/AdminServerApplication.java
@@ -1,11 +1,13 @@
 package com.saucesubfresh;
 
+import com.saucesubfresh.starter.security.annotation.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author lijunping on 2021/12/22
  */
+@EnableSecurity
 @SpringBootApplication
 public class AdminServerApplication {
 
